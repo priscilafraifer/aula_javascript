fbef63b3b8c91d59a2ab14d794a22fa2f7584022
priscilafraifer <priscilafraifer@gmail.com>
2021-09-06 20:17:35 -0300

    confirmar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-alert ("Eu amo você!!");
+/*
+function soma(){    
+    var x = prompt("Digite o valor de x: ");
+    var y = prompt("Digite o valor de y: "); 
+    x = parseInt(x);
+    y = parseInt(y);   
+    console.log("A soma dos valores digitados é " + (x+y));
+}
+*/
+
+function somavetor(ar){
+    var soma = 0;
+    for (var i = 0; i < ar.length; i++){
+        soma = soma + ar[i];
+    }
+    return soma;
+}
