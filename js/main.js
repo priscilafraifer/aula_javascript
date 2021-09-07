8433701df99cfdb9757b9639da8178a9eba37bd5
priscilafraifer <priscilafraifer@gmail.com>
2021-09-07 16:08:05 -0300

    confirmar

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/*
+
 function soma(){    
     var x = prompt("Digite o valor de x: ");
     var y = prompt("Digite o valor de y: "); 
@@ -6,7 +6,7 @@ function soma(){
     y = parseInt(y);   
     console.log("A soma dos valores digitados é " + (x+y));
 }
-*/
+
 
 function somavetor(ar){
     var soma = 0;
@@ -15,3 +15,17 @@ function somavetor(ar){
     }
     return soma;
 }
+
+function inverter(ar){
+    var lista = [];
+    for (var i = ar.length-1; i >= 0; i--){
+        lista.push(ar[i]);
+    }
+    return lista;    
+}
+
+function inverterfrase(){
+    x = "Meu nome é Priscila".split('').reverse().join(''); 
+    return x;  
+}
+
